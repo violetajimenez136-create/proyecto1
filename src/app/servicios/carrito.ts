554a50cc8ad431782c3a37787d39e2c21039bfed
violetajimenez136-create <violetajimenez136@gmail.com>
2026-05-27 10:55:46 -0300

    hicimos los metodos

--- src/app/servicios/carrito.ts
+++ src/app/servicios/carrito.ts
@@ -3,4 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Carrito {}
+export class Carrito {
+  obtenerProducto(): void {
+
+  }
+  añadirProducto(): void {
+
+  }
+  editarCantidad(): void {
+
+  }
+  eliminarProducto(): void {
+    
+  }
+}
