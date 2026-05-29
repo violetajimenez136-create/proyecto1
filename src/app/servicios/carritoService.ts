5daa8bd76eb7661615b24a9b0d9c163db74e3dbc
violetajimenez136-create <violetajimenez136@gmail.com>
2026-05-29 11:02:09 -0300

    hicimos el servicio de carrito

---
+++ src/app/servicios/carritoService.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Producto } from '../models/producto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class carritoService {
+  Carrito: Producto[] = [];
+
+añadirProducto(p: Producto): void {
+    this.Carrito.push(p);
+  }
+  obtenerProducto(): Producto []{
+    return this.Carrito;
+  }
+  
+  aumentarCantidad(id: number): void {
+
+    const producto = this.Carrito.find(p => p.id === id);
+    if (producto) {
+      producto.cantidad++;
+    }
+
+  }
+
+  disminuirCantidad(id: number): void{
+    const producto = this.Carrito.find(p => p.id === id);
+    if (producto && producto.cantidad > 1) {
+      producto.cantidad--;
+    }
+
+  }
+  eliminarProducto(id : number): void {
+this.Carrito = this.Carrito.filter(p => p.id !== id);
+   
+}
+}
